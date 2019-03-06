bd065056ffc5399ed21293c82ee91ea582f13d43
pmarkiewicz <piotr.markiewicz@gmail.com>
2019-03-06 08:58:59 +0100

    colors fixed

--- src/display.h
+++ src/display.h
@@ -5,8 +5,8 @@ void display_init(uint8_t no_of_leds);
 void display_set_color(uint8_t r, uint8_t g, uint8_t b);
 
 inline void display_red() { display_set_color(128, 0, 0); }
-inline void display_yellow() { display_set_color(0, 128, 0); }
-inline void display_green() { display_set_color(0, 0, 128); }
+inline void display_yellow() { display_set_color(128, 128, 0); }
+inline void display_green() { display_set_color(0, 128, 0); }
 inline void display_off() { display_set_color(0, 0, 0); }
 
 #endif
